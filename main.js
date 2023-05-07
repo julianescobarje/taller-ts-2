1de969e9d3681620221983e595e13e0247f54174
Julian Escobar <escobarjulian347@gmail.com>
2023-05-07 18:54:21 -0500

    Taller 1 finalizado

---
+++ main.js
@@ -0,0 +1,34 @@
+import { series } from './data.js';
+const table = document.querySelector('tbody');
+if (!table) {
+    throw new Error('No table found');
+}
+let seasonsSum = 0;
+series.forEach((serie) => {
+    const row = document.createElement('tr');
+    const idCell = document.createElement('th');
+    const nameCell = document.createElement('td');
+    const channelCell = document.createElement('td');
+    const seasonsCell = document.createElement('td');
+    row.classList.add('table-active');
+    idCell.textContent = serie.id.toString();
+    channelCell.textContent = serie.channel;
+    seasonsCell.textContent = serie.seasons.toString();
+    const link = document.createElement('a');
+    link.href = serie.link;
+    link.classList.add('text-decoration-none');
+    link.textContent = serie.name;
+    nameCell.appendChild(link);
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(channelCell);
+    row.appendChild(seasonsCell);
+    table.appendChild(row);
+    seasonsSum += serie.seasons;
+});
+const span = document.querySelector('caption span');
+if (!span) {
+    throw new Error('No span found');
+}
+const averageSeasons = seasonsSum / series.length;
+span.textContent = averageSeasons.toString();
